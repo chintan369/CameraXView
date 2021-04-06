2e3ae93e9402bf871caec5b2aed46f3594531b21
chintak369promact <chintak@promactinfo.com>
2021-04-06 12:33:31 +0530

    Fixes issue

--- CameraX/src/main/java/com/creative/camerax/analysis/BitmapProcessor.kt
+++ CameraX/src/main/java/com/creative/camerax/analysis/BitmapProcessor.kt
@@ -1,10 +1,11 @@
 package com.creative.camerax.analysis
 
-import android.graphics.Bitmap
+import android.graphics.*
+import android.media.Image
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
 import com.creative.camerax.helper.rotateOn
-import com.creative.camerax.helper.toBitmap
+import java.io.ByteArrayOutputStream
 
 typealias OnImageBitmapProcessListener = (processedBitmap: Bitmap) -> Unit
 
@@ -16,6 +17,25 @@ class BitmapProcessor(val bitmapProcessListener: OnImageBitmapProcessListener) :
         image.close()
         bitmap = bitmap.rotateOn(image.imageInfo.rotationDegrees.toFloat())
         bitmapProcessListener(bitmap)
+    }
+
+    private fun Image.toBitmap(): Bitmap {
+        val yBuffer = planes[0].buffer // Y
+        val vuBuffer = planes[2].buffer // VU
+
+        val ySize = yBuffer.remaining()
+        val vuSize = vuBuffer.remaining()
+
+        val nv21 = ByteArray(ySize + vuSize)
+
+        yBuffer.get(nv21, 0, ySize)
+        vuBuffer.get(nv21, ySize, vuSize)
+
+        val yuvImage = YuvImage(nv21, ImageFormat.NV21, this.width, this.height, null)
+        val out = ByteArrayOutputStream()
+        yuvImage.compressToJpeg(Rect(0, 0, yuvImage.width, yuvImage.height), 50, out)
+        val imageBytes = out.toByteArray()
+        return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
     }
 
 
